f9aa74f8876cf7955a76bdc333775b78e8b8818c
vicmafe <victormateuslf@gmail.com>
2021-04-26 09:39:33 -0300

    create routers, pages and globalstyle

--- app-queue-manager/src/App.js
+++ app-queue-manager/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import GlobalStyle from './Style';
+import GetPassword from './Pages/GetPassword';
+import Queue from './Pages/ManagerQueue';
+import Manager from './Pages/ManagerQueue';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalStyle>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ GetPassword } />
+          <Route exact path="/queue" component={ Queue } />
+          <Route exact path="/managerqueue" component={ Manager } />
+        </Switch>
+      </BrowserRouter>
+    </GlobalStyle>
   );
 }
 
---
+++ app-queue-manager/src/Pages/GetPassword/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GetPassword = () => {
+  return (
+    <>
+      <h1>Olá, seja bem-vindo!</h1>
+      <h3>Você é:</h3>
+    </>
+  );
+};
+
+export default GetPassword;
